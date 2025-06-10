c102f30010a18422129d1a2ce2cea31861f48040
KeleMRose <mabinekelebogile@gmail.com>
2025-06-10 22:41:31 +0100

    Lesson 4: AC button event handler .And  custom keydown listners

--- script.js
+++ script.js
@@ -20,7 +20,15 @@ function appendToDisplay(value) {
         display.value = value;
     } else if (currentValue ==='0' && value === '.') {
         display.value = currentValue + value;
-    } else {
+    } else if (value === '.') {
+        // Get the last number in the display
+        let lastNumber = currentValue.split('/[+\-*/]').pop();
+        // Only add the decimal if the current number doesnt have it
+        if (!lastNumber.includes('.')) {
+            display.value = currentValue + value
+        }
+
+    }else {
         display.value = currentValue + value;
     }
 
@@ -33,7 +41,13 @@ function appendToDisplay(value) {
 function clearDisplay() {
     console.log('Clear button pressed.');
 
-    alert('Clear button was clicked');
+    display.value = '0';
+    justCalculated = false;
+
+    display.style.backgroundColor = '#f0f0f0';
+    setTimeout(() => {
+        display.style.backgroundColor = '';
+    }, 150);
 }
 
 function deleteLast() {
@@ -48,7 +62,6 @@ function deleteLast() {
         display.value = currentValue.slice(0, -1);
     }
 
-    alert('Backspace button was clicked');
 }
 
 function calculate() {
@@ -56,6 +69,31 @@ console.log('Equals button pressed.');
 
     alert('Equals button was clicked');
 }
+
+document.addEventListener('keydown', function(event) {
+    console.log('key pressed', event.key);
+
+    if (event.key >= '0' && event.key <= '9') {
+        appendToDisplay(event.key);
+    } else if (event.key === '.') {
+        appendToDisplay('.');
+    } else if (event.key === '+') {
+        appendToDisplay('+');
+    } else if (event.key === '-') {
+        appendToDisplay('-')
+    } else if (event.key === '*') {
+        appendToDisplay('*') 
+    } else if (event.key === '/') {
+        appendToDisplay('/')
+    }
+    else if (event.key === 'Enter' || event.key === '=') {
+        calculate();
+    } else if (event.key === 'Escape'|| event.key === 'c' || event.key === 'C') {
+        clearDisplay(); 
+    } else if (event.key === 'Backspace') {
+        deleteLast();
+    }
+})
 
 document.addEventListener('DOMContentloaded', function() {
     console.log('Calculator loaded successfully');
